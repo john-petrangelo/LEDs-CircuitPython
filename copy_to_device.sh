7355877e6bb9788509723eb625e4e88d3cf5545c
john-petrangelo <john@petrangelo.org>
2023-12-13 23:38:25 -0500

    Added utils.py to deploy script

--- copy_to_device.sh
+++ copy_to_device.sh
@@ -1,6 +1,13 @@
 DEVICE=/Volumes/CIRCUITPY/
 FILES=("code.py")
-LIB_FILES=("colors.py" "roving_dot.py" "models.py" "renderer.py" "animations.py")
+LIB_FILES=(
+  "colors.py"
+  "roving_dot.py"
+  "models.py"
+  "renderer.py"
+  "animations.py"
+  "utils.py"
+)
 echo "Copying $LIB_FILES from src/ to device lib/"
 cd src
 cp $LIB_FILES $DEVICE/lib
