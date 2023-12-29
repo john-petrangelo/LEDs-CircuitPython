bb2229f88890bb9ad3a68b131ae44c210cb28e1a
john-petrangelo <john@petrangelo.org>
2023-12-29 16:50:02 -0500

    Moved most of the wx code into the renderer

--- src/luminaria/renderer/wx_renderer.py
+++ src/luminaria/renderer/wx_renderer.py
@@ -4,23 +4,26 @@ import wx
 
 from luminaria.models import Model
 
+PIXEL_GAP_WIDTH = 10
+PIXEL_MAX_WIDTH = 96
+
 
 class Renderer:
     """
       A class responsible for rendering a model in a wx Panel using wxPython.
 
       :param pixels_count: The number of pixels to light up
-      :param pixel_size: The diameter of the pixels to render
-      :param pixels_gap_width: The amount of horizontal space to leave between pixels
+      :param model: (optional) The model to render
       """
-
-    def __init__(self, pixels_count: int, pixel_size: int, pixels_gap_width: int):
+    def __init__(self, pixels_count: int, model: Model = None):
         self._pixels_count = pixels_count
-        self._pixel_size = pixel_size
-        self._pixels_gap_width = pixels_gap_width
         self._start_time = time.monotonic_ns() // 1000000
-        self._panel = None
-        self._model = None
+        self._model = model
+
+        # Create the frame/window
+        self._pixel_size, window_width = _calc_sizes(pixels_count)
+        self._frame = PixelDrawingFrame(self, window_width, self._pixel_size + 48, None,
+                                        title="Pixels Animation Simulator")
 
     @property
     def model(self):
@@ -30,11 +33,10 @@ class Renderer:
     def model(self, new_model: Model):
         self._model = new_model
 
-    def render(self):
+    def render(self, panel: wx.Panel):
         """
         Render the model at the current time and updates the LED lights accordingly.
         """
-
         # If there's no model then there is nothing to render.
         if self._model is None:
             print("No model to render")
@@ -46,21 +48,21 @@ class Renderer:
             return
 
         # Update the current state of the model to match the current time.
-        absolute_now_ms = time.monotonic()*1000
+        absolute_now_ms = time.monotonic() * 1000
         relative_now_ms = absolute_now_ms - self._start_time
         self._model.update(relative_now_ms)
 
         # Now draw each pixel
-        dc = wx.PaintDC(self._panel)
+        dc = wx.PaintDC(panel)
         for i in range(self._pixels_count):
             pos = i / (self._pixels_count - 1)
             color = self._model.render(pos)
-            x = (i+1)*self._pixels_gap_width + i*self._pixel_size
+            x = (i + 1) * PIXEL_GAP_WIDTH + i * self._pixel_size
             y = 10
 
             wx_color = wx.Colour(color[0], color[1], color[2])
             dc.SetBrush(wx.Brush(wx_color))
-            dc.DrawCircle(x + self._pixel_size//2, y + self._pixel_size//2, self._pixel_size//2)
+            dc.DrawCircle(x + self._pixel_size // 2, y + self._pixel_size // 2, self._pixel_size // 2)
 
     def reset(self):
         """
@@ -68,5 +70,46 @@ class Renderer:
         """
         self._start_time = time.monotonic_ns() // 1000000
 
-    def set_panel(self, panel: wx.Panel):
-        self._panel = panel
+
+def _calc_sizes(pixels_count: int):
+    screen_width, screen_height = wx.GetDisplaySize()
+
+    pixel_width = (screen_width - 2 * PIXEL_GAP_WIDTH - (pixels_count + 1) * PIXEL_GAP_WIDTH) // pixels_count
+    pixel_width = min(PIXEL_MAX_WIDTH, pixel_width)
+    window_width = (pixels_count + 1) * PIXEL_GAP_WIDTH + pixels_count * pixel_width
+    return pixel_width, window_width
+
+
+class PixelDrawingFrame(wx.Frame):
+    def __init__(self, renderer: Renderer, window_width: int, window_height: int, *args, **kw):
+        super().__init__(*args, **kw)
+
+        # Set up the frame with size, position, and background color
+        self.SetSize(window_width, window_height)
+        self.Centre()
+        self.SetBackgroundColour(wx.Colour(32, 32, 32))
+
+        # Create the panel where the pixels will be drawn
+        self.panel = PixelDrawingPanel(self, renderer)
+
+        # Set up a timer to update the pixels periodically
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)
+        self.timer.Start(10)  # ms
+
+        # Start the show
+        self.Refresh()
+        self.Show(True)
+
+    def on_timer(self, _):
+        self.Refresh()
+
+
+class PixelDrawingPanel(wx.Panel):
+    def __init__(self, parent: wx.Frame, renderer: Renderer):
+        super().__init__(parent)
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self._renderer = renderer
+
+    def on_paint(self, _):
+        self._renderer.render(self)
--- src/other/main_host.py
+++ src/other/main_host.py
@@ -5,77 +5,20 @@ from luminaria.colors import BLUE, GREEN, ORANGE, RED, VIOLET, YELLOW
 from luminaria.models import MultiGradient
 from luminaria.renderer.wx_renderer import Renderer
 
-PIXELS_COUNT = 100
-PIXEL_GAP_WIDTH = 10
-
-
-class PixelDrawingPanel(wx.Panel):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.Bind(wx.EVT_PAINT, self.on_paint)
-        self._renderer = None
-
-    def on_paint(self, _):
-        dc = wx.PaintDC(self)
-        # self.draw_pixels(dc)
-        self._renderer.render()
-
-    def set_renderer(self, renderer: Renderer):
-        self._renderer = renderer
-        renderer.set_panel(self)
-
-
-class PixelDrawingFrame(wx.Frame):
-    def __init__(self, renderer: Renderer, window_width: int, window_height: int, *args, **kw):
-        super().__init__(*args, **kw)
-
-        # Set up the frame with size, position, and background color
-        self.SetSize(window_width, window_height)
-        self.Centre()
-        self.SetBackgroundColour(wx.Colour(32, 32, 32))
-
-        # Create the panel where the pixels will be drawn
-        self.panel = PixelDrawingPanel(self)
-        self.panel.set_renderer(renderer)
-
-        # Set up a timer to update the pixels periodically
-        self.timer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)
-        self.timer.Start(10) # ms
-
-        # Start the show
-        self.Refresh()
-        self.Show(True)
-
-    def on_timer(self, _):
-        self.Refresh()
-
-
-def calc_sizes():
-    screen_width, screen_height = wx.GetDisplaySize()
-
-    pixel_width = (screen_width - 20 - (PIXELS_COUNT + 1) * PIXEL_GAP_WIDTH) // PIXELS_COUNT
-    pixel_width = min(96, pixel_width)
-    window_width = (PIXELS_COUNT + 1) * PIXEL_GAP_WIDTH + PIXELS_COUNT * pixel_width
-    return pixel_width, window_width
+PIXELS_COUNT = 24
 
 
 if __name__ == "__main__":
     # Create the wx app object
     app = wx.App(False)
 
-    # Figure out window size as well as the size of the pixels to draw
-    pixel_size, window_w = calc_sizes()
-    print(f"pixel_size={pixel_size}  window_width={window_w}")
-
-    # Set up the model for the pixels
-    print("Setting up model")
-    wx_renderer = Renderer(PIXELS_COUNT, pixel_size, PIXEL_GAP_WIDTH)
+    # Set up the model defining the pixel colors
+    print("Setting up model rendering")
     gradient = MultiGradient("Gradient rainbow", [RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, RED])
     rotate = Rotate("Rotation", 2500, gradient)
+    wx_renderer = Renderer(PIXELS_COUNT, rotate)
     wx_renderer.model = rotate
 
-    # Launch the app window
-    print("Opening window")
-    frame = PixelDrawingFrame(wx_renderer, window_w, pixel_size + 48, None, title="Pixels Animation Simulator")
+    # Run the app
+    print("App loop starting")
     app.MainLoop()
